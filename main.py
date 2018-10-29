65456408f270d370edeb649dd15cb3a2becae63b
Gabriel Madonna <gmadonna@mit.edu>
2018-10-28 21:38:52 -0400

    gotta make the norm

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from model import KerasLSTM
+from model import RNN
 from preprocess import gen_letter_dict, partition, to_matrices
 from utils import *
 
@@ -9,10 +9,10 @@ def main():
     letters_train, letters_test = partition(letters, ratio=.2)
     X_train, y_train = to_matrices(letters_train)
     X_test, y_test = to_matrices(letters_train)
-    model = KerasLSTM()
-    model.generate()
+    model = RNN()
+    model.generate(hidden_size=50, timesteps=50, features=3, output_dim=20, layers=1)
     model.train(X_train, y_train)
-    model.test(X_test, y_test, plot=True)
+    model.test(X_test, y_test)
 
 
 if __name__ == '__main__':
