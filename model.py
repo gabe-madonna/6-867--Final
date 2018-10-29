ee0803b499055d763a3adcc664a853c6557ebc7d
pkarnati2004 <pkarnati@mit.edu>
2018-10-28 20:35:34 -0400

    Creating model

--- model.py
+++ model.py
@@ -1,5 +1,15 @@
 import keras
 
-model = Sequential()
-## add 100 LSTMs
-## add Softmax activation
+
+class KerasLSTM:
+
+    def generate(self, hidden_size, timesteps, features, output_dim):
+        model = Sequential()
+        # 50 for number of timesteps, 3 for features
+        model.add(LSTM(hidden_size, input_shape=(timesteps, features)))
+        # dense takes in output dimensionality
+        model.add(Dense(output_dim))
+        model.add(Activation('softmax'))
+        model.compile(loss='categorical_crossentropy', optimizer='sgd')
+
+        return model
