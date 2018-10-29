7b21757cf3d546e5955c688f671f11b325635a13
pkarnati2004 <pkarnati@mit.edu>
2018-10-28 20:59:25 -0400

    Create basic RNN architecture

--- model.py
+++ model.py
@@ -5,26 +5,37 @@ from keras.layers import LSTM
 
 import numpy as np
 
-class KerasLSTM:
+class RNN:
+
+    def __init__(self):
+        """
+        Initialize the Sequential Model
+        """
+        self.model = Sequential()
 
     def generate(self, hidden_size, timesteps, features, output_dim, layers):
-        model = Sequential()
+        """
+        Generate RNN defined
+        """
         # 50 for number of timesteps, 3 for features
-        model.add(LSTM(hidden_size, input_shape=(timesteps, features)))
+        self.model.add(LSTM(hidden_size, input_shape=(timesteps, features)))
         # dense takes in output dimensionality
-        model.add(Dense(output_dim))
+        self.model.add(Dense(output_dim))
         # add softmax activation
-        model.add(Activation('softmax'))
+        self.model.add(Activation('softmax'))
         # indicate loss and optimizer
-        model.compile(loss='categorical_crossentropy', optimizer='sgd')
+        self.model.compile(loss='categorical_crossentropy', optimizer='sgd')
 
         return model
 
     def train(self, model, train_x, train_y, test_x, test_y, epochs):
+        """
+        Train the model
+        """
         # train the model
         # validation_data=(test_x, test_y),
-        fit = model.fit(train_x, train_y, epochs=epochs, verbose=2, shuffle=False)
-        # return trained
-        return fit
+        history = self.model.fit(train_x, train_y, epochs=epochs, verbose=2, shuffle=False)
+        
+        return history
 
     def test():
