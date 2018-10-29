9ebd5112e2b9689427520f85a9154cf557a38210
pkarnati2004 <pkarnati@mit.edu>
2018-10-28 21:08:35 -0400

    documentation

--- model.py
+++ model.py
@@ -8,15 +8,20 @@ import numpy as np
 class RNN:
 
     def __init__(self):
-        """
-        Initialize the Sequential Model
-        """
+        '''
+        initialize the Sequential Model
+        '''
         self.model = Sequential()
 
     def generate(self, hidden_size, timesteps, features, output_dim, layers):
-        """
-        Generate RNN defined
-        """
+        '''
+        generate RNN defined
+        :hidden_size (int): number of units in each hidden layer (LSTM)
+        :timesteps (int): number of timesteps per sample
+        :features (int): number of features each sample has
+        :output_dim (int): number of dimensions in the output i.e. number of classes
+        :layers (int): number of layers
+        '''
         # 50 for number of timesteps, 3 for features
         self.model.add(LSTM(hidden_size, input_shape=(timesteps, features)))
         # dense takes in output dimensionality
@@ -28,9 +33,11 @@ class RNN:
         return model
 
     def train(self, model, train_x, train_y, epochs):
-        """
-        Train the model
-        """
+        '''
+        train the model
+        :train_x (np.array): inputs (x)
+        :train_y (np.array): outputs (y)
+        '''
         # train the model
         # validation_data=(test_x, test_y),
         history = self.model.fit(train_x, train_y, epochs=epochs, verbose=2, shuffle=False)
@@ -38,6 +45,9 @@ class RNN:
         return history
 
     def test(test_X, test_Y):
+        '''
+        test the model
+        '''
         # make a prediction
         yhat = model.predict(test_X)
         test_X = test_X.reshape((test_X.shape[0], test_X.shape[2]))
